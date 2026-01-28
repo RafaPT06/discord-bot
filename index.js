c5da1d3d34f26ff3f20f47b344b8f3975ab3b05a
atuaprima <136728815+olaaaaaaaaaaaaaaaa@users.noreply.github.com>
2026-01-28 10:37:36 +0000

    Update footer and author mention in messages

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.listen(PORT, "0.0.0.0", () => {
 const OWNER_ID = process.env.OWNER_ID;
 const CHANNEL_ID = process.env.CHANNEL_ID; // fallback if DB not set
 const TZ = process.env.TZ || "Europe/Lisbon";
+const owner = `Rafa (<@${OWNER_ID}>)`;
 
 // =======================
 // FILES
@@ -361,7 +362,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
         inline: false,
       }
     )
-    .setFooter({ text: "Made by Rafa @(atuaprima_)" })
+    .setFooter({ text: `Made by ${owner}` })
   if (isGuild && owner) {
     embed.addFields({
       name: "🚀 Deploy Updates (Owner Only)",
@@ -388,7 +389,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
       const startedAt = Math.floor((Date.now() - process.uptime() * 1000) / 1000);
       const msg = [
         `⏱️ **Uptime:** <t:${startedAt}:R>`,
-        `👨‍💻 **Made by:** Rafa @(atuaprima_)`,
+        `👨‍💻 **Made by:** ${owner}`,
       ].join("\n");
       return interaction.reply({ content: msg, ephemeral: false });
     }
