85474847146feab2c5382c759787f1665e4bf32d
unknown <136728815+RafaPT06@users.noreply.github.com>
2026-07-17 15:57:10 +0100

    Install Meowz-created role listing

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
 require('./src/web/roleCreationPatch').install();
 require('./src/web/roleDeletionPatch').install();
+require('./src/web/dashboardRoleListPatch').install();
 require('./src/index');
